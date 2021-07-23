f0e7d1f08255c2890d498b831fec5581f845debe
Lucas Corte <corte1994.lc@gmail.com>
2021-07-23 16:45:28 -0300

    implementaçao do esventos

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,20 @@
+//initial data
 let currentColor = 'black';
 
+let screen = document.querySelector('#tela');
+let screenContext = screen.getContext('2d');
+
+//events
 const colors = document.querySelectorAll('.colorArea .color');
 colors.forEach(item => {
   item. addEventListener('click', colorClickEvent);
 })
 
+screen.addEventListener('mousedown', mouseDownEvent);
+screen.addEventListener('mousemove', mouseMoveEvent);
+screen.addEventListener('mouseup', mouseUpEvent);
+
+//functions
 function colorClickEvent(e) {
   let color = e.target.getAttribute('data-color');
   currentColor = color;
