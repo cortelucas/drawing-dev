d673fd6bf84d4fa10e869728033ae70bbea7c7c3
Lucas Corte <corte1994.lc@gmail.com>
2021-07-23 17:18:31 -0300

    implementaçao a funçao mouseDOnwevent

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,8 @@
 //initial data
 let currentColor = 'black';
+let canDraw = false;
+let mouseX = 0;
+let mouseY = 0;
 
 let screen = document.querySelector('#tela');
 let screenContext = screen.getContext('2d');
@@ -8,11 +11,14 @@ let screenContext = screen.getContext('2d');
 const colors = document.querySelectorAll('.colorArea .color');
 colors.forEach(item => {
   item. addEventListener('click', colorClickEvent);
-})
+});
 
 screen.addEventListener('mousedown', mouseDownEvent);
 screen.addEventListener('mousemove', mouseMoveEvent);
 screen.addEventListener('mouseup', mouseUpEvent);
+
+const btnClear = document.querySelector('.clear');
+btnClear.addEventListener('click', clearScreen);
 
 //functions
 function colorClickEvent(e) {
@@ -21,4 +27,43 @@ function colorClickEvent(e) {
 
   document.querySelector('.color.active').classList.remove('active');
   e.target.classList.add('active');
+}
+
+function mouseDownEvent(e) {
+  canDraw = true;
+
+  mouseX = e.pageX - screen.offsetLeft;
+  mouseY = e.pageY - screen.offsetTop;
+}
+
+function mouseMoveEvent(e) {
+  if(canDraw) {
+    draw(e.pageX, e.pageY)
+  }
+}
+
+function mouseUpEvent() {
+  canDraw = false;
+}
+
+function draw(x, y) {
+  let pointX = x - screen.offsetLeft;
+  let pointY = y - screen.offsetTop;
+
+  screenContext.beginPath();
+  screenContext.lineWidth = 4;
+  screenContext.lineJoin = 'round';
+  screenContext.moveTo(mouseX, mouseY);
+  screenContext.lineTo(pointX, pointY);
+  screenContext.closePath();
+  screenContext.strokeStyle = currentColor;
+  screenContext.stroke();
+
+  mouseX = pointX;
+  mouseY = pointY;
+}
+
+function clearScreen() {
+  screenContext.setTransform(1, 0, 0, 1, 0, 0);
+  screenContext.clearRect(0, 0, screenContext.canvas.width, screenContext.canvas.height);
 }
